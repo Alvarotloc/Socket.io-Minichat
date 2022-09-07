50b300445cc6827204e14c9a77110d4e048a3d44
Alvarotloc <alvarotloc@gmail.com>
2022-09-07 18:45:06 +0200

    Añadido tailwind y creado un css básico para el front

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ app.use(express.static(__dirname + `/public`));
 const PORT = 4000 | process.env.PORT;
 
 
-io.on('connection', () => { 
-    console.log('Cliente contectado')
- });
-
+io.on('connection', (socket) => { 
+    socket.emit('bienvenido-luis-angel',{msg : 'Bienvenido Luis Ángel',fecha : new Date()});
+    socket.on('mensaje-cliente',(data) => {
+        console.log(data)
+    })
+});
 
 server.listen(PORT,() => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
